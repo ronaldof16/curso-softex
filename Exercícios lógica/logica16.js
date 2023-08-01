026853a3e7f1cddb95caaa8afa43d3fd498408f1
ronaldof16 <ronaldof16@hotmail.com>
2023-08-01 14:05:06 -0300

    exercício 16

---
+++ Exercícios lógica/logica16.js
@@ -0,0 +1,14 @@
+nomes = ["Ronaldo", "José", "Alex", "Pedro", "Bob", "Ana", "Júlia", "Maria", "Duda", "Carla"];
+idades = [33, 15, 12, 35, 45, 28, 6, 19, 26, 53];
+cores = ["azul", "verde", "marrom", "preto", "cinza", "rosa", "roxo", "lilás", "amarelo", "laranja"];
+
+console.log(nomes);
+console.log(idades);
+console.log(cores);
+
+cores[0] = "preto";
+idades[9] = 100;
+
+console.log(nomes);
+console.log(idades);
+console.log(cores);
