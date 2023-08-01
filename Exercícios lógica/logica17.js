8f37634b456514d29b1cf2be0a9b1d67e4bafe11
ronaldof16 <ronaldof16@hotmail.com>
2023-08-01 15:29:35 -0300

    exrcício 17

---
+++ Exercícios lógica/logica17.js
@@ -0,0 +1,10 @@
+animais = [
+    ["leão", "mamífero", "10 anos"],
+    ["cobra", "réptil", "15 anos"],
+    ["sapo", "anfíbio", "3 anos"],
+    ["tubarão", "peixe", "6 anos"]
+];
+
+console.log(animais);
+
+
