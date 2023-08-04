d06d14fae8e6ed904fd397dec0c898b6e30f86c7
ronaldof16 <ronaldof16@hotmail.com>
2023-08-04 13:46:44 -0300

    exercício 20

---
+++ Exercícios lógica/logica20.js
@@ -0,0 +1,7 @@
+vetor = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7];
+
+for(let i = 0;i < vetor.length; i++) {
+    if(vetor[i] === 20) {
+        console.log(`O número 20 está na posição ${i} da lista`);
+    }
+}
