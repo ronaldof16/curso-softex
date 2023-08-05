8299b2b801fa35a2712383e3f6a983156548f7aa
ronaldof16 <ronaldof16@hotmail.com>
2023-08-05 10:50:12 -0300

    exercício 19

---
+++ Exercícios lógica/logica19.js
@@ -0,0 +1,8 @@
+pessoas = [
+    ["José",  "38 anos", "Pai do Lucas"],
+    ["Luana", "26 anos", "Mãe do Pedro"],
+    ["Ana", "19 anos", "Mãe da Alice"],
+    ["Alex", "22 anos", "Pai do Bruno"]
+];
+
+console.log(pessoas);
