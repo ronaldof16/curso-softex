991bae88ad3a8d41a6c43e84693786914cbc6849
ronaldof16 <ronaldof16@hotmail.com>
2023-09-26 00:04:55 -0300

    Resolução do exercício 02 da plataforma

---
+++ Exercícios javascript/plataforma02/script.js
@@ -0,0 +1,7 @@
+let nota1 = Number(prompt("Digite a primeira nota: "));
+let nota2 = Number(prompt("Digite a segunda nota: "));
+let nota3 = Number(prompt("Digite a terceira nota: "));
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log(`A média das três notas é ${media.toFixed(2)}`);
