ccd885d4dea083ac119fd0f539f5a2ff1071d738
ronaldof16 <ronaldof16@hotmail.com>
2023-09-25 23:54:07 -0300

    resolução do exercício 01 da plataforma

---
+++ Exercícios javascript/plataforma01/script.js
@@ -0,0 +1,24 @@
+/* Cadastro 01 */
+
+let nome = "José";
+let salario = 2000;
+let idade = 33;
+let possuiDiploma = true;
+
+document.write("Nome: " + nome + "<br>");
+document.write("Salário: " + salario + "<br>");
+document.write("Idade: " + idade + "<br>");
+document.write("Possui diploma? " + possuiDiploma + "<br><br>");
+
+
+/* Cadastro 0 */
+
+let nome1 = "Paulo";
+let salario1 = 1500;
+let idade1 = 21;
+let possuiDiploma1 = false;
+
+document.write("Nome: " + nome1 + "<br>");
+document.write("Salário: " + salario1 + "<br>");
+document.write("Idade: " + idade1 + "<br>");
+document.write("Possui diploma? " + possuiDiploma1);
